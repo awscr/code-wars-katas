4249281b72c415b401d65d9da3e43869e5b8f1a3
awscr <05742p@proton.me>
2023-03-09 07:26:20 -0800

    Add completed 8kyu katas

---
+++ 8kyu/array_plus_array.js
@@ -0,0 +1,25 @@
+/* https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+Array plus array
+
+Instructions:
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+// My solution
+function arrayPlusArray(arr1, arr2) {
+  return (
+    arr1.reduce((total, num) => {
+      return (total += num);
+    }, 0) +
+    arr2.reduce((total, num) => {
+      return (total += num);
+    }, 0)
+  );
+}
+
+// Trials
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); //21
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])); //-21
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600])); //2100
---
+++ 8kyu/grasshopper_messi_goals_function.js
@@ -0,0 +1,30 @@
+/* https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+Grasshopper - Messi goals function
+
+Instructions:
+Messi goals function
+
+Messi is a soccer player with goals in three leagues:
+
+    LaLiga
+    Copa del Rey
+    Champions
+
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+// My solution
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Trials
+console.log(goals(5, 10, 2)); //17
+console.log(goals(0, 0, 0)); //0
+console.log(goals(10, 0, 5)); //15
