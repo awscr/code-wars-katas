63e3e8f87275ab37c0dfbd7aaf68f7f80bf4cdb4
awscr <05742p@proton.me>
2023-04-28 07:59:24 -0700

    Add completed 8kyu katas

---
+++ 8kyu/function_2_squaring_an_argument.js
@@ -0,0 +1,16 @@
+/* https://www.codewars.com/kata/523b623152af8a30c6000027
+Function 2 - Squaring an Argument
+
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// My solution
+function square(n) {
+  return n * n;
+}
+
+// Trials
+console.log(square(9)); //81
+console.log(square(1)); //1
+console.log(square(-6)); //-36
