0a0158df2293293cc9ba826789f168da90a60425
awscr <05742p@proton.me>
2023-07-10 21:03:14 -0700

    Add completed 8kyu katas

---
+++ 8kyu/grasshopper_combine_strings.js
@@ -0,0 +1,16 @@
+/* https://www.codewars.com/kata/55f73f66d160f1f1db000059
+Grasshopper - Combine strings
+
+Instructions:
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+*/
+
+// My solution
+function combineNames(str1, str2) {
+  return str1 + " " + str2;
+}
+
+// Trials
+console.log(combineNames("James", "Stevens")); //"James Stevens"
+console.log(combineNames("Maria", "Lopez")); //"Maria Lopez"
+console.log(combineNames("Code", "Wars")); //"Code Wars"
