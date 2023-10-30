01dfaedb92b0b66ae27d8d9ad4cccd5071fef66d
awscr <05742p@proton.me>
2023-10-30 08:34:29 -0700

    Add completed 8kyu katas

---
+++ 8kyu/flick_switch.js
@@ -0,0 +1,32 @@
+/* https://www.codewars.com/kata/64fbfe2618692c2018ebbddb
+Flick Switch
+
+Instructions:
+Create a function that always returns true for every item in a given list. However, if an element is the word "flick", switch to always returning the 
+opposite boolean value.
+Examples
+
+['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+
+Notes
+
+    "flick" will always be given in lowercase.
+    A list may contain multiple flicks.
+    Switch the boolean value on the same element as the flick itself.
+*/
+
+// My solution
+function flickSwitch(arr) {
+  let flick = true;
+
+  return arr.map((item) => (item === "flick" ? (flick = !flick) : flick));
+}
+
+// Trials
+console.log(flickSwitch(["codewars", "flick", "code", "wars"])); //[true, false, false, false]
+console.log(flickSwitch(["bicycle", "jarmony", "flick", "sheep", "flick"])); //[true, true, false, false, true]
+console.log(flickSwitch(["flick"])); //[false]
---
+++ 8kyu/neutralisation.js
@@ -0,0 +1,51 @@
+/* https://www.codewars.com/kata/65128732b5aff40032a3d8f0
+Instructions:
+Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+    When positives and positives interact, they remain positive.
+    When negatives and negatives interact, they remain negative.
+    But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+Worked Example
+
+("+-+", "+--") ➞ "+-0"
+# Compare the first characters of each string, then the next in turn.
+# "+" against a "+" returns another "+".
+# "-" against a "-" returns another "-".
+# "+" against a "-" returns "0".
+# Return the string of characters.
+
+Examples
+
+("--++--", "++--++") ➞ "000000"
+
+("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+
+("-++-", "-+-+") ➞ "-+00"
+
+Notes
+
+The two strings will be the same length.
+*/
+
+// My solution
+function neutralise(s1, s2) {
+  let str = "";
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] === "+" && s2[i] === "+") {
+      str += "+";
+    } else if (s1[i] === "-" && s2[i] === "-") {
+      str += "-";
+    } else {
+      str += "0";
+    }
+  }
+
+  return str;
+}
+
+// Trials
+console.log(neutralise("--++--", "++--++")); //"000000"
+console.log(neutralise("--++", "++++")); //"00++"
+console.log(neutralise("---++-+--", "-+++--++-")); //"-00+0-+0-"
