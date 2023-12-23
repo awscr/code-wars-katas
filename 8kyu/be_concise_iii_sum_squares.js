9d1110107ddfe30c2bfca5e8dd41f3b97ae5815a
awscr <05742p@proton.me>
2023-12-22 23:31:29 -0800

    Add completed 8kyu katas

---
+++ 8kyu/be_concise_iii_sum_squares.js
@@ -0,0 +1,28 @@
+/* https://www.codewars.com/kata/56f8fe6a2e6c0dc83b0008a7
+Be Concise III - Sum Squares
+
+Instructions:
+You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array. For example:
+
+sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+sumSquares([7,3,9,6,5]) === 200
+sumSquares([11,13,15,18,2]) === 843
+
+Shorten the code such that it meets the requirements.
+
+A few hints:
+
+    Try researching about built-in Array methods; they may help shorten your code a lot
+
+Good luck!
+*/
+
+// My solution
+function sumSquares(arr) {
+  return arr.reduce((sum, curr) => sum + curr ** 2, 0);
+}
+
+// Trials
+console.log(sumSquares([1, 2, 3, 4, 5])); //55
+console.log(sumSquares([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])); //110
+console.log(sumSquares([11, 13, 15, 18, 2])); //843
